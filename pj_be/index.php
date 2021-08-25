2e2a7782e84a2e067a059dd9b75d532fa33fab05
NatswarChuan <natswarchuan@gmail.com>
2021-08-25 10:05:00 +0700

    25/8 update index & controller

--- pj_be/index.php
+++ pj_be/index.php
@@ -1,10 +1,10 @@
 <?php
-define("BASE_URL", "127.0.0.1/pj_be1/pj_be");
+define("BASE_URL", "C:/wamp64/www/pj_be1/pj_be/");
 
-include_once './app/config/config.php';
+include_once BASE_URL . '/app/config/config.php';
 
 spl_autoload_register(function($className) {
-    include_once './app/models/'.$className . '.php';
+    include_once BASE_URL . '/app/models/'.$className . '.php';
 });
 
-include_once './app/controllers/Controllers.php';
+include_once BASE_URL . '/app/controllers/Controllers.php';
