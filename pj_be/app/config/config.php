1cb1cfd37adcdca08751114f97a35b9fddcbfdd6
cootham <khiemtran1412@gmail.com>
2021-08-28 15:17:24 +0700

    28/08 update

--- pj_be/app/config/config.php
+++ pj_be/app/config/config.php
@@ -4,4 +4,4 @@ define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'data_project');
-define('DB_PORT', '3306');
+define('DB_PORT', '3308');
--- pj_be/index.php
+++ pj_be/index.php
@@ -16,3 +16,4 @@ $request_url = !empty($_GET['url']) ? '/' . $_GET['url'] : '/';
 
 $method_url = !empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
 $router->map($request_url, $method_url);
+
