0d5fff3ebe36d1b0c8c2f7648f5d59bf5215ebd9
weez-yan <quyenlaso6@gmail.com>
2021-08-28 19:22:30 +0700

    28/8 UPLOAD

--- pj_be/app/views/header.php
+++ pj_be/app/views/header.php
@@ -7,5 +7,8 @@
     <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/fonts/simple-line-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
     <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/smoothproducts.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" >
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/style.css">
 </head>
--- pj_be/app/views/navbar.php
+++ pj_be/app/views/navbar.php
@@ -7,6 +7,23 @@
                 <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing</a></li>
                 <li class="nav-item"><a class="nav-link" href="about-us.html">About Us</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact-us.html">Contact Us</a></li>
+                <div class="box">
+                    <div class="container-1">
+                        <span class="icon"><i class="fa fa-search"></i></span>
+                        <input type="search" id="search" placeholder="Search..." />
+                    </div>
+                    <button id="cart">
+                        <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+                        
+                    </button>
+                    <div id="myModal" class="modal">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title"></h5>
+                                <span class="close">&times;</span>
+                            </div>
+                            
+                        </div>
             </ul>
         </div>
     </div>
